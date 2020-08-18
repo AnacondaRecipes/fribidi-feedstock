6a57526eadbee4568e1f7624fe7b4dbb4b989646
Kai Tietz <ktietz@anaconda.com>
2020-08-18 16:22:15 +0200

    Fix Windows buildwq

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,9 @@
 setlocal EnableDelayedExpansion
 @echo on
 
+:: Copy file so that later on Ninja is happy too
+COPY %BUILD_PREFIX%\Scripts\meson-script.py %BUILD_PREFIX%\Scripts\meson
+
 :: meson options
 :: (set pkg_config_path so deps in host env can be found)
 set ^"MESON_OPTIONS=^
@@ -12,8 +15,7 @@ set ^"MESON_OPTIONS=^
  ^"
 
 :: configure build using meson
-%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson setup builddir %MESON_OPTIONS%
-:: python -m %BUILD_PREFIX%\Scripts\meson setup builddir !MESON_OPTIONS!
+%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson setup builddir !MESON_OPTIONS!
 if errorlevel 1 exit 1
 
 :: print results of build configuration
